d59bd8f4433e9c2f4d9a246a095461a491ce2893
Sean Gollos <sean.gollos@gmail.com>
2021-05-26 19:44:18 -0700

    Transition from outside to inside, zoom on cases functionality finished.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] GameObject outsideCam;
+    [SerializeField] GameObject insideCam;
+
+    //Serialized for testing
+    [SerializeField] bool playerInsideStore = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        outsideCam.SetActive(true);
+        insideCam.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerInsideStore)
+        {
+            outsideCam.SetActive(false);
+            insideCam.SetActive(true);
+        }else
+        {
+            outsideCam.SetActive(true);
+            insideCam.SetActive(false);
+        }
+    }
+
+    //Meant to be called once the player is successfully in the store
+    public void doorIsOpen()
+    {
+        playerInsideStore = true;
+    }
+}
---
+++ Assets/Scripts/CaseSelect.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaseSelect : MonoBehaviour
+{
+    [SerializeField] Camera insideCam;
+    Animator camAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camAnim = insideCam.GetComponent<Animator>();
+        camAnim.SetBool("TableFramed", false);
+    }
+
+    private void OnMouseDown()
+    {
+        if (!camAnim.GetBool("TableFramed"))
+        {
+            camAnim.SetTrigger(gameObject.name);
+            camAnim.SetBool("TableFramed", true);
+        }else
+        {
+            camAnim.SetTrigger("BackToDefault");
+            camAnim.SetBool("TableFramed", false);
+        }
+    }
+}
