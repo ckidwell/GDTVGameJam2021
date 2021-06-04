a116b55470444fff5b5d7c88c1c6683d1efc31eb
Sean Gollos <sean.gollos@gmail.com>
2021-06-03 17:29:52 -0700

    Added score to game over screen

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    GameController gameController;
+    TextMeshProUGUI scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = FindObjectOfType<GameController>();
+        scoreText = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = gameController.score.ToString();
+    }
+}
